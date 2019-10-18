a850c0db9cda6c8459895cc216ad6fcb87e55608
dartagnan45 <dartagnan45@gmail.com>
2019-10-18 16:41:39 +0200

    correction de bugs

--- index.php
+++ index.php
@@ -1,45 +1,48 @@
 <?php include('inc/head.php'); ?>
 
 <?php
-if (isset($_POST['content'])){
-    $document = "../files/roswell".$_POST["file"];
-    $file= fopen($document,'w');
-    fwrite($file,$_POST['content']);
+if (isset($_POST["content"])) {
+    $file = "../files/roswell" . $_POST["file"];
+    $file = fopen($file, "w");
+    fwrite($file, ($_POST["content"]));
     fclose($file);
 }
-
-$dir = opendir("../php_files_handling_ressources/files")or die('erreur');
-$content = file_get_contents($document);
-while($file = readdir($dir)){
-    if(!in_array($file,array(".",".."))){
-        echo '<a href="?f='.$file.'">';
-        echo $file;
-        echo '</a>';
-    }
-    if (isset($_POST['delete'])) {
-        unlink("../files/roswell/" . $_GET['f']);
-        header('location: index.php');
-    }
-}
-if(isset($_GET["f"])){
-    echo "<h1 style='font-size: 25px;'>{$_GET["f"]}</h1>";
-    $file = "../files/roswell/".$_GET["f"];
-    $content = file_get_contents($file);
-};
 ?>
+    <div class="content">
+        <h1 style="font-size: 30px; text-align: center;">Choose the page you wish to edit</h1>
 
-    <form method="post" action="">
-        <div class="form-group">
-            <label>
-                        <textarea name="content" style="width: 30em; height: 10em;">
-                           <?php echo $content; ?>
-                        </textarea>
-            </label>
-        </div>
-        <input type="hidden" name="file" value="<?= $_GET["f"] ?> >
-        <input type="submit" value="Envoyer">
-    </form>
+<?php
+        $dir = opendir("../files/roswell");
+        while ($file = readdir($dir)) {
+            if (!in_array($file, array(".", ".."))) {
+                echo '<div style="text-align: center;"><a href="?f=' . $file . '">';
+                echo $file;
+                echo '</a></div>';
+            }
+            if (isset($_POST['delete'])) {
+                unlink("../files/roswell/" . $_GET['f']);
+                header('location: index.php');
+            }
+        }
 
-
+        if (isset($_GET["f"])) {
+            echo "<h1 style='font-size: 25px;'>{$_GET["f"]}</h1>";
+            $file = "../files/roswell/" . $_GET["f"];
+            $content = file_get_contents($file);
+?>
+            <form method="POST" action="index.php">
+                <label>
+                <textarea name="contenu" style="width:100%;height:200px">
+                        <?php echo $content; ?>
+                </textarea>
+                </label>
+                <input type="hidden" name="file" value="<?= $_GET["f"] ?> ">
+                <input type="submit" value="Envoyer"/>
+                <input type="submit" class="btn-danger btn-md" name="delete" value="Delete">
+            </form>
+<?php
+        }
+?>
+    </div>
 
 <?php include('inc/foot.php'); ?>
