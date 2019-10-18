486befb5cc00ac0a9fcabdc6fcfe94a6e158931c
dartagnan45 <dartagnan45@gmail.com>
2019-10-18 16:48:05 +0200

    correction d'autres bugs

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php
 if (isset($_POST["content"])) {
-    $file = "../files/roswell" . $_POST["file"];
+    $file = "../php_files_handling_ressources/files/roswell" . $_POST["file"];
     $file = fopen($file, "w");
     fwrite($file, ($_POST["content"]));
     fclose($file);
@@ -12,7 +12,7 @@ if (isset($_POST["content"])) {
         <h1 style="font-size: 30px; text-align: center;">Choose the page you wish to edit</h1>
 
 <?php
-        $dir = opendir("../files/roswell");
+        $dir = opendir("../php_files_handling_ressources/files/roswell");
         while ($file = readdir($dir)) {
             if (!in_array($file, array(".", ".."))) {
                 echo '<div style="text-align: center;"><a href="?f=' . $file . '">';
@@ -20,25 +20,27 @@ if (isset($_POST["content"])) {
                 echo '</a></div>';
             }
             if (isset($_POST['delete'])) {
-                unlink("../files/roswell/" . $_GET['f']);
+                unlink("../php_files_handling_ressources/files/roswell/" . $_GET['f']);
                 header('location: index.php');
             }
         }
 
         if (isset($_GET["f"])) {
             echo "<h1 style='font-size: 25px;'>{$_GET["f"]}</h1>";
-            $file = "../files/roswell/" . $_GET["f"];
+            $file = "../php_files_handling_ressources/files/roswell/" . $_GET["f"];
             $content = file_get_contents($file);
 ?>
             <form method="POST" action="index.php">
                 <label>
-                <textarea name="contenu" style="width:100%;height:200px">
+                <textarea name="contenu" style="width:200%;height:300px">
                         <?php echo $content; ?>
                 </textarea>
                 </label>
                 <input type="hidden" name="file" value="<?= $_GET["f"] ?> ">
+                <div>
                 <input type="submit" value="Envoyer"/>
                 <input type="submit" class="btn-danger btn-md" name="delete" value="Delete">
+                </div>
             </form>
 <?php
         }
